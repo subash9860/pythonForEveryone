86a3b2620fd9d997f4792174a4da74ea19693a42
subash9860 <subaazkc@gmail.com>
2021-02-24 00:20:37 +0545

    9.4 assignment

--- 9.4.py
+++ 9.4.py
@@ -10,3 +10,20 @@
 name = input("Enter file:")
 if len(name) < 1 : name = "mbox-short.txt"
 handle = open(name)
+counts = dict()
+for line in handle:
+    if line.startswith("From "):
+        wds = line.split()
+        words = wds[1]
+        counts[words] = counts.get(words,0) + 1
+
+bigcount = None
+bigword = None
+for word, count in counts.items():
+    if bigcount is None or count > bigcount:
+        bigword = word
+        bigcount = count
+
+print(bigword, bigcount)
+
+
